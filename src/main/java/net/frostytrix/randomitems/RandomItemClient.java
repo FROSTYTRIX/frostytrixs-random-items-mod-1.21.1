26703ca79c137d4e5950fe0151686df26f44f40f
FROSTYTRIX <frostytrix13@gmail.com>
2024-10-27 20:15:41 +0100

    Initial commit

---
+++ src/main/java/net/frostytrix/randomitems/RandomItemClient.java
@@ -0,0 +1,10 @@
+package net.frostytrix.randomitems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RandomItemClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/frostytrix/randomitems/RandomItemsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.frostytrix.randomitems;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RandomItemsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
